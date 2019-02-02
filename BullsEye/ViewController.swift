c7d26e0432332b352138832b3bdb7b17dd315e8e
Nick Romanenko <ruras.dalgard@gmail.com>
2019-02-02 23:11:16 +0200

    Добавили простую кнопку с простым действием.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        // создаем сам алерт-контроллер
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        
+        // создаем "действие" для алерт-контроллера (в данном случае кнопку "Awesome", которая просто закрывает уведомление)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        // добавляем в наш алерт-контроллер созданное ранее "действие"
+        alert.addAction(action)
+        
+        // показываем(активируем на экране) наш созданный алерт-контроллер
+        present(alert, animated: true, completion: nil)
+        
         
     }
 }
