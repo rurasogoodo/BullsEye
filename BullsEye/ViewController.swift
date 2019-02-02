21313c7f511b796403d79ef292b31d749312797c
Nick Romanenko <ruras.dalgard@gmail.com>
2019-02-02 19:18:59 +0200

    Начало работы над игрой.

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Nick_Romanenko on 2/2/19.
+//  Copyright © 2019 Nick_Romanenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+        
+    }
+}
+
