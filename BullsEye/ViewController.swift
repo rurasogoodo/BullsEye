5a7b7f923a32825a9ec784fb2026cb2cd2f682eb
Nick Romanenko <ruras.dalgard@gmail.com>
2019-02-04 11:58:41 +0200

    Добавлена возможность получить рандомные значения, настроены нужные для этого алутлеты

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    var targetValue = 0
     var currentValue: Int = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        
+        startNewRound() //начальная установка рандомного значения и слайдера
     }
 
     @IBAction func showAlert() {
         // сообщение для пользователя о том, какое значение сейчас имеет слайдер
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         // создаем сам алерт-контроллер и добавляем в него наше сообщение
         let alert = UIAlertController(title: "Hi...", message: message, preferredStyle: .alert)
@@ -33,12 +38,27 @@ class ViewController: UIViewController {
         // показываем(активируем на экране) наш созданный алерт-контроллер
         present(alert, animated: true, completion: nil)
         
-        
+        // начинаем новый раунд, устанавливая новые значения рандома и слайдера
+        startNewRound()
     }
     
+    // вызывается, когда пользователь "использует" слайдер
     @IBAction func sliderMoved(_ slider: UISlider) {
         // ловим значение слайдера и округляем его для нашей переменной
         currentValue = lroundf(slider.value)
+    }
+    
+    // обновляем значения для нового раунда
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
